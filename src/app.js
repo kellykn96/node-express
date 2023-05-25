1ec4257bfc0148fdd071677c71d0c2cbc7f0dd23
Kelly Knoblauch <kellyKnobla@gmail.com>
2023-05-25 13:39:27 -0300

    primeiro post: add outra fruta dentro de frutas

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express();
+app.use(express.json()) //post interpretar json para adicionar atributos
 
 const frutas = [
     {id: 1, "fruta": "maçã"},
@@ -8,11 +9,21 @@ const frutas = [
 ]
 
 app.get('/', (req,res) => {
-    res.status(200).send('ok'); 
+    res.status(200).send('ok');
 })
 
 app.get('/frutas', (req, res) => {
     res.status(200).json(frutas)
 })
+
+app.post('/frutas', (req,res) =>{
+    frutas.push(req.body);
+    res.status(201).send('Fruta cadastrada')
+}) //ir em postman fazer um get de http://localhost:3000/frutas e um post dele também.
+//o post precisa ter um body - raw - json (app.use)
+/*no body de post, add um objeto {
+    "id": 3,
+    "fruta": "laranja"
+} */
 
 export default app
