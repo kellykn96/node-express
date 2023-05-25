379b26f793b24cd1464af0cc95f5fadd6b67b62d
Kelly Knoblauch <kellyKnobla@gmail.com>
2023-05-25 14:36:05 -0300

    primeiro put, atualizando por id

--- src/app.js
+++ src/app.js
@@ -8,6 +8,10 @@ const frutas = [
     {id: 2, "fruta": "banana"}
 ]
 
+function buscaFruta(id) {
+    return frutas.findIndex(fruta => fruta.id == id)
+}
+
 app.get('/', (req,res) => {
     res.status(200).send('ok');
 })
@@ -15,15 +19,28 @@ app.get('/', (req,res) => {
 app.get('/frutas', (req, res) => {
     res.status(200).json(frutas)
 })
-
-app.post('/frutas', (req,res) =>{
-    frutas.push(req.body);
-    res.status(201).send('Fruta cadastrada')
-}) //ir em postman fazer um get de http://localhost:3000/frutas e um post dele também.
+//ir em postman fazer um get de http://localhost:3000/frutas e um post dele também.
 //o post precisa ter um body - raw - json (app.use)
 /*no body de post, add um objeto {
     "id": 3,
     "fruta": "laranja"
 } */
+
+app.post('/frutas', (req,res) =>{
+    frutas.push(req.body);
+    res.status(201).send('Fruta cadastrada')
+})
+
+app.put('/frutas/:id', (req, res) => {
+    let index = buscaFruta(req.params.id);
+    frutas[index].fruta = req.body.fruta;
+    res.json(frutas)
+})
+/* ir no potman fazer put http://localhost:3000/frutas/3 ou outro id , e escrever no body raw json apenas o objeto com fruta e a nova fruta: {
+    "fruta":"abacaxi"
+}
+Ao fazer o get /frutas perceberá que alterou a fruta do id 3
+*/
+
 
 export default app
